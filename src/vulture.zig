4f697274791514753b8b1b389bd10ea404d99d45
Luiz Alberto Portilho Braga <portilho.braga@gmail.com>
2023-12-28 19:45:14 -0300

    rename things

--- src/vulture.zig
+++ src/vulture.zig
@@ -6,164 +6,164 @@ const http = std.http;
 const sw = std.mem.startsWith;
 const eql = std.mem.eql;
 
-const Zure = struct {
-    const RouteFunc = *const fn (*Context) anyerror!void;
+const Vulture = @This();
 
-    const Route = struct {
-        method: http.Method,
-        handleFunc: RouteFunc,
-    };
+const RouteFunc = *const fn (*Context) anyerror!void;
 
-    const Context = struct {
-        req: *http.Server.Request,
-        res: *http.Server.Response,
-        params: ?std.StringHashMap([]const u8) = null,
-        arena: std.heap.ArenaAllocator,
+const Route = struct {
+    method: http.Method,
+    handleFunc: RouteFunc,
+};
 
-        // TODO parse params
-        fn init(child_alloc: std.mem.Allocator, req: *http.Server.Request, res: *http.Server.Response) Context {
-            return .{
-                .res = res,
-                .req = req,
-                .arena = std.heap.ArenaAllocator.init(child_alloc),
-            };
-        }
+const Context = struct {
+    params: ?std.StringHashMap([]const u8) = null,
+    arena: std.heap.ArenaAllocator,
+    res: *http.Server.Response,
+    req: *http.Server.Request,
 
-        fn deinit(ctx: *Context) void {
-            ctx.arena.deinit();
-        }
-
-        fn send(c: *Context, string: []const u8) !void {
-            c.res.transfer_encoding = .{ .content_length = string.len };
-            try c.res.headers.append("content-type", "text/plain");
-            try c.res.headers.append("connection", "close");
-            try c.res.send();
-            try c.res.writeAll(string);
-        }
-
-        fn sendJson(c: *Context, json_struct: anytype) !void {
-            const allocator = c.arena.allocator();
-
-            const json_string = try std.json.stringifyAlloc(allocator, json_struct, .{});
-            errdefer allocator.free(json_string);
-            c.res.transfer_encoding = .{ .content_length = json_string.len };
-            try c.res.headers.append("content-type", "application/json");
-            try c.res.headers.append("connection", "close");
-            try c.res.send();
-            try c.res.writeAll(json_string);
-        }
-
-        fn sendStatus(c: *Context, status: http.Status) !void {
-            c.res.status = status;
-            try c.res.headers.append("connection", "close");
-            try c.res.send();
-        }
-
-        fn setStatus(c: *Context, status: http.Status) *Context {
-            c.res.status = status;
-            return c;
-        }
-
-        fn parseParams(c: *Context) !?std.StringHashMap([]const u8) {
-            // /route?
-            const target = c.req.target;
-
-            var n = std.mem.count(u8, target, "?");
-
-            if (n > 1) return error.ParseError1;
-            if (n == 0 or n > 1) return null;
-
-            const allocator = c.arena.allocator();
-
-            var params = std.StringHashMap([]const u8).init(allocator);
-            n = std.mem.indexOf(u8, c.target, "?").? + 1;
-
-            var iter = std.mem.tokenizeScalar(u8, target[n..], "&");
-            while (iter.next()) |param| {
-                n = std.mem.indexOf(u8, param, "=");
-                const name = param[0..n];
-                const value = param[n + 1 ..];
-                try params.put(name, value);
-            }
-
-            return params;
-        }
-    };
-
-    server: http.Server,
-    ally: std.mem.Allocator,
-    routes: std.StringHashMap(Route),
-
-    fn init(ally: std.mem.Allocator) Zure {
+    // TODO parse params
+    fn init(child_alloc: std.mem.Allocator, req: *http.Server.Request, res: *http.Server.Response) Context {
         return .{
-            .ally = ally,
-            .server = http.Server.init(ally, .{ .reuse_port = true, .reuse_address = true }),
-            .routes = std.StringHashMap(Route).init(ally),
+            .res = res,
+            .req = req,
+            .arena = std.heap.ArenaAllocator.init(child_alloc),
         };
     }
 
-    fn deinit(self: *Zure) void {
-        self.server.deinit();
-        self.routes.deinit();
+    fn deinit(ctx: *Context) void {
+        ctx.arena.deinit();
     }
 
-    fn newRoute(self: *Zure, method: http.Method, target: []const u8, handleFunc: RouteFunc) !void {
-        try self.routes.put(target, .{ .method = method, .handleFunc = handleFunc });
+    fn send(c: *Context, string: []const u8) !void {
+        try c.write(string);
     }
 
-    fn get(self: *Zure, target: []const u8, handleFunc: RouteFunc) !void {
-        try self.routes.put(target, .{ .method = .GET, .handleFunc = handleFunc });
+    fn sendJson(c: *Context, json_struct: anytype) !void {
+        const allocator = c.arena.allocator();
+        const json_string = try std.json.stringifyAlloc(allocator, json_struct, .{});
+        errdefer allocator.free(json_string);
+        try c.write(json_string);
     }
 
-    fn post(self: *Zure, target: []const u8, handleFunc: RouteFunc) !void {
-        try self.routes.put(target, .{ .method = .POST, .handleFunc = handleFunc });
+    /// Send the bytes and eval the header
+    fn write(c: *Context, bytes: []const u8) !void {
+        c.res.transfer_encoding = .{ .content_length = bytes.len };
+        try c.res.headers.append("content-type", "application/json");
+        try c.res.headers.append("connection", "close");
+        try c.res.send();
+        try c.res.writeAll(bytes);
     }
 
-    fn listen(self: *@This(), address: []const u8, port: u16) !void {
-        const addr = try net.Address.parseIp4(address, port);
+    fn sendStatus(c: *Context, status: http.Status) !void {
+        c.res.status = status;
+        try c.res.headers.append("connection", "close");
+        try c.res.send();
+    }
 
-        try self.server.listen(addr);
+    fn setStatus(c: *Context, status: http.Status) *Context {
+        c.res.status = status;
+        return c;
+    }
 
-        outer: while (true) {
-            var response = try self.server.accept(.{ .allocator = self.ally });
-            defer response.deinit();
+    fn parseParams(c: *Context) !?std.StringHashMap([]const u8) {
+        // /route?
+        const target = c.req.target;
 
-            while (response.reset() != .closing) {
-                try response.wait();
+        var n = std.mem.count(u8, target, "?");
 
-                const route = self.routes.get(response.request.target) orelse {
-                    response.status = .bad_request;
-                    try response.headers.append("connection", "close");
-                    try response.send();
-                    try response.finish();
-                    break :outer;
-                };
+        if (n > 1) return error.ParseError1;
+        if (n == 0 or n > 1) return null;
 
-                var c = Zure.Context.init(self.ally, &response.request, &response);
-                defer c.deinit();
+        const allocator = c.arena.allocator();
 
-                try route.handleFunc(&c);
-                try response.finish();
-            }
+        var params = std.StringHashMap([]const u8).init(allocator);
+        n = std.mem.indexOf(u8, c.target, "?").? + 1;
+
+        var iter = std.mem.tokenizeScalar(u8, target[n..], "&");
+        while (iter.next()) |param| {
+            n = std.mem.indexOf(u8, param, "=");
+            const name = param[0..n];
+            const value = param[n + 1 ..];
+            try params.put(name, value);
         }
+
+        return params;
     }
 };
 
-fn testRoutGetSend(c: *Zure.Context) anyerror!void {
+server: http.Server,
+ally: std.mem.Allocator,
+routes: std.StringHashMap(Route),
+
+fn init(ally: std.mem.Allocator) Vulture {
+    return .{
+        .ally = ally,
+        .server = http.Server.init(ally, .{ .reuse_port = true, .reuse_address = true }),
+        .routes = std.StringHashMap(Route).init(ally),
+    };
+}
+
+fn deinit(self: *Vulture) void {
+    self.server.deinit();
+    self.routes.deinit();
+}
+
+fn newRoute(self: *Vulture, method: http.Method, target: []const u8, handleFunc: RouteFunc) !void {
+    try self.routes.put(target, .{ .method = method, .handleFunc = handleFunc });
+}
+
+fn get(self: *Vulture, target: []const u8, handleFunc: RouteFunc) !void {
+    try self.routes.put(target, .{ .method = .GET, .handleFunc = handleFunc });
+}
+
+fn post(self: *Vulture, target: []const u8, handleFunc: RouteFunc) !void {
+    try self.routes.put(target, .{ .method = .POST, .handleFunc = handleFunc });
+}
+
+fn listen(self: *Vulture, address: []const u8, port: u16) !void {
+    const addr = try net.Address.parseIp4(address, port);
+
+    try self.server.listen(addr);
+
+    outer: while (true) {
+        var response = try self.server.accept(.{ .allocator = self.ally });
+        defer response.deinit();
+
+        while (response.reset() != .closing) {
+            try response.wait();
+
+            const route = self.routes.get(response.request.target) orelse {
+                response.status = .bad_request;
+                try response.headers.append("connection", "close");
+                try response.send();
+                try response.finish();
+                break :outer;
+            };
+
+            var c = Vulture.Context.init(self.ally, &response.request, &response);
+            defer c.deinit();
+
+            try route.handleFunc(&c);
+            try response.finish();
+        }
+    }
+}
+
+fn testRoutGetSend(c: *Vulture.Context) anyerror!void {
     if (c.req.headers.contains("Authorization")) {
         std.debug.print("ok", .{});
     }
     try c.send("Hello Word");
 }
 
-fn testRoutGetSendJson(c: *Zure.Context) anyerror!void {
+fn testRoutGetSendJson(c: *Vulture.Context) anyerror!void {
     if (c.req.headers.contains("Authorization")) {
         std.debug.print("ok", .{});
     }
     try c.sendJson(.{ .msg = "Hello Word" });
 }
 
-fn testRoutGetBadRequestSendJson(c: *Zure.Context) anyerror!void {
+fn testRoutGetBadRequestSendJson(c: *Vulture.Context) anyerror!void {
     if (c.req.headers.contains("Authorization")) {
         std.debug.print("ok", .{});
     }
@@ -173,7 +173,7 @@ fn testRoutGetBadRequestSendJson(c: *Zure.Context) anyerror!void {
 test "main" {
     const ally = std.testing.allocator;
 
-    var app = Zure.init(ally);
+    var app = Vulture.init(ally);
     defer app.deinit();
 
     try app.newRoute(.GET, "/test1", testRoutGetSend);
